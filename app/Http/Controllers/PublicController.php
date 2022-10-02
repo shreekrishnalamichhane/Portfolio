f04644090cba0a9847cd8c22bd0baa834b1ae733
shreekrishnalamichhane <uniksk123@gmail.com>
2022-10-02 16:34:12 +0545

    Frontend: Meta tags value implemented

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -12,73 +12,19 @@ class PublicController extends Controller
 {
     public function home()
     {
-        $page['title'] = "";
 
-        $page['meta'] = [
-            // General
-            ['name' => 'title',
-                'content' => ''],
-            ['name' => 'description',
-                'content' => ''],
-            ['name' => 'keywords',
-                'content' => ''],
-            ['name' => 'author',
-                'content' => ''],
-            ['name' => 'copyright',
-                'content' => ''],
-            ['name' => 'robots',
-                'content' => ''],
+        $var['name'] = get_setting('user_name');
+        $var['title'] = $var['name'] . " | Portfolio";
+        $var['locale'] = 'en_US';
+        $var['description'] = get_setting('user_bio');
+        $var['keywords'] = get_setting('user_keywords');
 
-            // OpenGraph
-            ['property' => 'og:title',
-                'content' => 'Title here'],
-            ['property' => 'og:locale',
-                'content' => ''],
-            ['property' => 'og:description',
-                'content' => ''],
-            ['property' => 'og:url',
-                'content' => ''],
-            ['property' => 'og:site_name',
-                'content' => ''],
-            ['property' => 'og:type',
-                'content' => ''],
-            ['property' => 'og:image',
-                'content' => ''],
-            ['property' => 'og:image:secure_url',
-                'content' => ''],
-            ['property' => 'og:image:width',
-                'content' => ''],
-            ['property' => 'og:image:height',
-                'content' => ''],
-            ['property' => 'og:image:alt',
-                'content' => ''],
-            ['property' => 'og:image:type',
-                'content' => ''],
-
-            // Twitter
-            ['name' => 'twitter:card',
-                'content' => ''],
-            ['name' => 'twitter:title',
-                'content' => ''],
-            ['name' => 'twitter:description',
-                'content' => ''],
-            ['name' => 'twitter:site',
-                'content' => ''],
-            ['name' => 'twitter:creater',
-                'content' => ''],
-            ['name' => 'twitter:image',
-                'content' => ''],
-            ['name' => 'twitter:image:alt',
-                'content' => ''],
-
-            // Google+
-            ['itemprop' => 'name',
-                'content' => ''],
-            ['itemprop' => 'description',
-                'content' => ''],
-            ['itemprop' => 'image',
-                'content' => ''],
-        ];
+        $image_info = getimagesize(get_setting('user_avatar'));
+        $var['avatar'] = get_public_path() . get_setting('user_avatar');
+        $var['avatar_width'] = $image_info[0];
+        $var['avatar_height'] = $image_info[1];
+        $var['avatar_mime'] = $image_info['mime'];
+        $var['avatar_alt'] = $var['name'] . "'s avatar image";
 
         $data['sociallinks'] = SocialLink::orderBy('order', 'ASC')->get();
         $data['techstacks'] = TechStack::orderBy('order', 'ASC')->get();
@@ -86,154 +32,247 @@ class PublicController extends Controller
         $data['workhistories'] = WorkHistory::orderBy('order', 'ASC')->get();
         $data['projects'] = Project::orderBy('created_at', 'DESC')->take(5)->get();
 
+        $page['title'] = $var['title'];
+
+        $page['meta'] = [
+            // General
+            ['name' => 'title',
+                'content' => $var['title']],
+            ['name' => 'description',
+                'content' => $var['description']],
+            ['name' => 'keywords',
+                'content' => $var['keywords']],
+            ['name' => 'author',
+                'content' => $var['name']],
+            ['name' => 'copyright',
+                'content' => $var['name']],
+            ['name' => 'robots',
+                'content' => 'index,follow'],
+            // OpenGraph
+            ['property' => 'og:title',
+                'content' => $var['title']],
+            ['property' => 'og:locale',
+                'content' => $var['locale']],
+            ['property' => 'og:description',
+                'content' => $var['description']],
+            ['property' => 'og:url',
+                'content' => get_current_path()],
+            ['property' => 'og:site_name',
+                'content' => $var['title']],
+            ['property' => 'og:type',
+                'content' => 'document'],
+            ['property' => 'og:image',
+                'content' => $var['avatar']],
+            ['property' => 'og:image:secure_url',
+                'content' => $var['avatar']],
+            ['property' => 'og:image:width',
+                'content' => $var['avatar_width']],
+            ['property' => 'og:image:height',
+                'content' => $var['avatar_height']],
+            ['property' => 'og:image:alt',
+                'content' => $var['avatar_alt']],
+            ['property' => 'og:image:type',
+                'content' => $var['avatar_mime']],
+
+            // Twitter
+            ['name' => 'twitter:card',
+                'content' => 'summary_large_image'],
+            ['name' => 'twitter:title',
+                'content' => $var['title']],
+            ['name' => 'twitter:description',
+                'content' => $var['description']],
+            ['name' => 'twitter:site',
+                'content' => get_setting('user_twitter_handle')],
+            ['name' => 'twitter:creater',
+                'content' => get_setting('user_twitter_handle')],
+            ['name' => 'twitter:image',
+                'content' => $var['avatar']],
+            ['name' => 'twitter:image:alt',
+                'content' => $var['avatar_alt']],
+
+            // Google+
+            ['itemprop' => 'name',
+                'content' => $var['title']],
+            ['itemprop' => 'description',
+                'content' => $var['description']],
+            ['itemprop' => 'image',
+                'content' => $var['avatar']],
+        ];
+
         return view('frontend.home', compact(['data', 'page']));
     }
 
     public function projects_index()
     {
-        $page['title'] = "";
+        $var['name'] = "Projects | " . get_setting('user_name');
+        $var['title'] = $var['name'] . " | Portfolio";
+        $var['locale'] = 'en_US';
+        $var['description'] = "Projects | " . get_setting('user_bio');
+        $var['keywords'] = get_setting('user_keywords');
+
+        $image_info = getimagesize(get_setting('user_avatar'));
+        $var['avatar'] = get_public_path() . get_setting('user_avatar');
+        $var['avatar_width'] = $image_info[0];
+        $var['avatar_height'] = $image_info[1];
+        $var['avatar_mime'] = $image_info['mime'];
+        $var['avatar_alt'] = $var['name'] . "'s avatar image";
+
+        $data['projects'] = Project::orderBy('created_at', 'DESC')->get();
+
+        $page['title'] = $var['title'];
 
         $page['meta'] = [
             // General
             ['name' => 'title',
-                'content' => ''],
+                'content' => $var['title']],
             ['name' => 'description',
-                'content' => ''],
+                'content' => $var['description']],
             ['name' => 'keywords',
-                'content' => ''],
+                'content' => $var['keywords']],
             ['name' => 'author',
-                'content' => ''],
+                'content' => $var['name']],
             ['name' => 'copyright',
-                'content' => ''],
+                'content' => $var['name']],
             ['name' => 'robots',
-                'content' => ''],
-
+                'content' => 'index,follow'],
             // OpenGraph
             ['property' => 'og:title',
-                'content' => 'Title here'],
+                'content' => $var['title']],
             ['property' => 'og:locale',
-                'content' => ''],
+                'content' => $var['locale']],
             ['property' => 'og:description',
-                'content' => ''],
+                'content' => $var['description']],
             ['property' => 'og:url',
-                'content' => ''],
+                'content' => get_current_path()],
             ['property' => 'og:site_name',
-                'content' => ''],
+                'content' => $var['title']],
             ['property' => 'og:type',
-                'content' => ''],
+                'content' => 'document'],
             ['property' => 'og:image',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['property' => 'og:image:secure_url',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['property' => 'og:image:width',
-                'content' => ''],
+                'content' => $var['avatar_width']],
             ['property' => 'og:image:height',
-                'content' => ''],
+                'content' => $var['avatar_height']],
             ['property' => 'og:image:alt',
-                'content' => ''],
+                'content' => $var['avatar_alt']],
             ['property' => 'og:image:type',
-                'content' => ''],
+                'content' => $var['avatar_mime']],
 
             // Twitter
             ['name' => 'twitter:card',
-                'content' => ''],
+                'content' => 'summary_large_image'],
             ['name' => 'twitter:title',
-                'content' => ''],
+                'content' => $var['title']],
             ['name' => 'twitter:description',
-                'content' => ''],
+                'content' => $var['description']],
             ['name' => 'twitter:site',
-                'content' => ''],
+                'content' => get_setting('user_twitter_handle')],
             ['name' => 'twitter:creater',
-                'content' => ''],
+                'content' => get_setting('user_twitter_handle')],
             ['name' => 'twitter:image',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['name' => 'twitter:image:alt',
-                'content' => ''],
+                'content' => $var['avatar_alt']],
 
             // Google+
             ['itemprop' => 'name',
-                'content' => ''],
+                'content' => $var['title']],
             ['itemprop' => 'description',
-                'content' => ''],
+                'content' => $var['description']],
             ['itemprop' => 'image',
-                'content' => ''],
+                'content' => $var['avatar']],
         ];
 
-        $data['projects'] = Project::orderBy('created_at', 'DESC')->get();
         return view('frontend.pages.projects.index', compact(['page', 'data']));
     }
 
     public function projects_show($slug)
     {
-        $page['title'] = "";
+        $data['project'] = Project::where('slug', '=', $slug)->firstOrFail();
+
+        $var['name'] = get_setting('user_name');
+        $var['title'] = $data['project']->title . " | Project | " . $var['name'];
+        $var['locale'] = 'en_US';
+        $var['description'] = $data['project']->short_description;
+        $var['keywords'] = $data['project']->keywords;
+
+        $image_info = getimagesize($data['project']->cover_img);
+        $var['avatar'] = get_public_path() . $data['project']->cover_img;
+        $var['avatar_width'] = $image_info[0];
+        $var['avatar_height'] = $image_info[1];
+        $var['avatar_mime'] = $image_info['mime'];
+        $var['avatar_alt'] = "Project image of " . $data['project']->title;
+
+        $page['title'] = $var['title'];
 
         $page['meta'] = [
             // General
             ['name' => 'title',
-                'content' => ''],
+                'content' => $var['title']],
             ['name' => 'description',
-                'content' => ''],
+                'content' => $var['description']],
             ['name' => 'keywords',
-                'content' => ''],
+                'content' => $var['keywords']],
             ['name' => 'author',
-                'content' => ''],
+                'content' => $var['name']],
             ['name' => 'copyright',
-                'content' => ''],
+                'content' => $var['name']],
             ['name' => 'robots',
-                'content' => ''],
-
+                'content' => 'index,follow'],
             // OpenGraph
             ['property' => 'og:title',
-                'content' => 'Title here'],
+                'content' => $var['title']],
             ['property' => 'og:locale',
-                'content' => ''],
+                'content' => $var['locale']],
             ['property' => 'og:description',
-                'content' => ''],
+                'content' => $var['description']],
             ['property' => 'og:url',
-                'content' => ''],
+                'content' => get_current_path()],
             ['property' => 'og:site_name',
-                'content' => ''],
+                'content' => $var['title']],
             ['property' => 'og:type',
-                'content' => ''],
+                'content' => 'document'],
             ['property' => 'og:image',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['property' => 'og:image:secure_url',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['property' => 'og:image:width',
-                'content' => ''],
+                'content' => $var['avatar_width']],
             ['property' => 'og:image:height',
-                'content' => ''],
+                'content' => $var['avatar_height']],
             ['property' => 'og:image:alt',
-                'content' => ''],
+                'content' => $var['avatar_alt']],
             ['property' => 'og:image:type',
-                'content' => ''],
+                'content' => $var['avatar_mime']],
 
             // Twitter
             ['name' => 'twitter:card',
-                'content' => ''],
+                'content' => 'summary_large_image'],
             ['name' => 'twitter:title',
-                'content' => ''],
+                'content' => $var['title']],
             ['name' => 'twitter:description',
-                'content' => ''],
+                'content' => $var['description']],
             ['name' => 'twitter:site',
-                'content' => ''],
+                'content' => get_setting('user_twitter_handle')],
             ['name' => 'twitter:creater',
-                'content' => ''],
+                'content' => get_setting('user_twitter_handle')],
             ['name' => 'twitter:image',
-                'content' => ''],
+                'content' => $var['avatar']],
             ['name' => 'twitter:image:alt',
-                'content' => ''],
+                'content' => $var['avatar_alt']],
 
             // Google+
             ['itemprop' => 'name',
-                'content' => ''],
+                'content' => $var['title']],
             ['itemprop' => 'description',
-                'content' => ''],
+                'content' => $var['description']],
             ['itemprop' => 'image',
-                'content' => ''],
+                'content' => $var['avatar']],
         ];
 
-        $data['project'] = Project::where('slug', '=', $slug)->firstOrFail();
         return view('frontend.pages.projects.show', compact(['page', 'data']));
     }
 }
